440db2afbd1b003af5b25ca25657ecd88164aacd
TaaviM88Laptop <juomakuski@hotmail.com>
2018-08-17 19:46:56 +0300

    Alustus

---
+++ Assets/Taavi stuff/Scripts/Door_Animation_Controller.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Animation_Controller : MonoBehaviour {
+
+public bool doorIsOpen = false;
+public int _doorIsClosedAngle = 0;
+public  int _doorIsOpenAngle = 90;
+bool _isPlayerNear = false;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		if(doorIsOpen == false)
+		{
+			transform.rotation = Quaternion.Euler(transform.rotation.x,_doorIsClosedAngle, transform.rotation.z);
+		}
+		else if (doorIsOpen == true)
+		{
+			transform.rotation = Quaternion.Euler(transform.rotation.x,_doorIsOpenAngle, transform.rotation.z);
+		}
+			
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(_isPlayerNear == true)
+		{
+			if(doorIsOpen == false)
+			{
+				if(Input.GetButtonDown("Fire1"))
+				{
+					OpenDoor();
+				}		
+			}
+
+			else if (doorIsOpen == true)
+			{
+				if(Input.GetButtonDown("Fire1"))
+				{		
+					CloseDoor();
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player")
+		{
+			_isPlayerNear = true;
+			Debug.Log("Player is near the door");
+		}
+		
+    }
+	void OnTriggerExit(Collider other) {
+        if(other.gameObject.tag == "Player")
+		{
+			_isPlayerNear = false;
+			Debug.Log("Player is not near the door");
+		}
+    }
+	public void OpenDoor()
+	{
+	
+		for(int i = _doorIsClosedAngle; i <= _doorIsOpenAngle; i++)
+		{		
+			transform.rotation = Quaternion.Euler(transform.rotation.x,  i, transform.rotation.z);
+			
+			if(i <= _doorIsOpenAngle)
+			{
+				doorIsOpen =true;
+			}
+		}
+	}
+
+	public void CloseDoor()
+	{	
+		for(int i = _doorIsOpenAngle; i >= _doorIsClosedAngle; i--)
+		{
+			transform.rotation = Quaternion.Euler(transform.rotation.x, i, transform.rotation.z);
+			if(i <= _doorIsClosedAngle)
+			{
+				doorIsOpen = false;
+			}
+		}				
+	}
+}
---
+++ Assets/Taavi stuff/Scripts/MoveToNavMesh.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class MoveToNavMesh : MonoBehaviour {
+
+public Transform goal;
+NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		 agent = GetComponent<NavMeshAgent>();
+		 FindTarget();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(transform.position.x == goal.position.x && transform.position.y == goal.position.y);
+			{
+				FindTarget();
+			}
+
+		/*if(Input.GetButtonDown("Submit"))
+		{		
+			salli tämä jos haluat itse kontrolloida milloin kohde päättää
+			päivittää kohteensa sijainnin.
+		}*/
+
+			
+	}
+
+	void FindTarget()
+	{
+		Debug.Log("Päivitetään kohteen sijainti");
+		agent.destination = goal.position;
+	}
+}
